7dd57fe684c8452926ef67f9b60128e17c33c212
Julien Lecomte <julien@cappysan.dev>
2026-03-24 12:12:43 +0100

    Move files

--- apk/CONTROL/start-hook.sh
+++ apk/CONTROL/start-hook.sh
@@ -2,25 +2,21 @@
 # SPDX-License-Identifier: MIT
 #
 # ------------------------------------------------------------------------------
-. /usr/local/AppCentral/cappysan-certbot/.env.install
+. /usr/local/AppCentral/cappysan-pi-hole/.env.install
 cd ${APKG_PKG_DIR:-/nonexistent} || exit 1
 . ${APKG_PKG_DIR}/env
 
 # Env
 # ===
-cat ${APKG_PKG_DIR}/conf.dist/version > ${APKG_CFG_DIR}/version
-
 # Beware of symlinks
-cat ${APKG_CFG_DIR}/custom.env ${APKG_CFG_DIR}/version >> ${APKG_CFG_DIR}/.env
+cat ${APKG_CFG_DIR}/custom.env ${APKG_PKG_DIR}/version > ${APKG_CFG_DIR}/.env
 chmod 640 ${APKG_CFG_DIR}/.env ${APKG_CFG_DIR}/custom.env
 
 
 # Dependencies
 # ============
 # Apache calls certbot and persistence, but it sets DOCKER_NO_RELOAD=1
-# so call certbot a second time.
 /usr/local/AppCentral/cappysan-apache/CONTROL/start-stop.sh reload
-/usr/local/AppCentral/cappysan-certbot/CONTROL/start-stop.sh reload
 
 
 # SSL
