3650ed8e56295cff5a0fca303ce3fcfb4aae2f4f
Julien Lecomte <julien@cappysan.dev>
2026-06-21 10:40:42 +0200

    Minor changes

--- apk/CONTROL/pre-uninstall.sh
+++ apk/CONTROL/pre-uninstall.sh
@@ -14,13 +14,15 @@ if test "x${APKG_PKG_STATUS}" != "xupgrade"; then
   # Remove Apache proxy config (our own deps.d copy; apache start-hook will
   # then no longer pick it up, and a reload clears it from sites-enabled/available)
   logger "[${WHAT}] Removing Apache pi-hole configuration..."
-  rm -f ${APKG_CFG_DIR}/deps.d/apache/sites-enabled/pi-hole.conf
-  rm -f ${APKG_CFG_DIR}/deps.d/apache/sites-available/pi-hole.conf
-  /usr/local/AppCentral/cappysan-apache/CONTROL/start-stop.sh reload >/dev/null 2>&1
+  rm -f /share/Configuration/apache/sites-*/pi-hole.conf
+  rm -f /share/Configuration/pi-hole/deps.d/apache/sites-*/pi-hole.conf
 
   # Remove certbot renewal hook
   logger "[${WHAT}] Removing certbot renewal hook..."
+  rm -f /share/Configuration/certbot/letsencrypt/renewal-hooks/deploy/10-pi-hole
   rm -f /share/Configuration/pi-hole/deps.d/certbot/renewal-hooks/deploy/10-pi-hole
+
+  /usr/local/AppCentral/cappysan-apache/CONTROL/start-stop.sh reload >/dev/null 2>&1
 fi
 
 exit 0
